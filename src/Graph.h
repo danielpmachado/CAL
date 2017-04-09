d3b2bd5638374b66dbd9f0f0794a624a660a1026
Kyahra <sofiacbahamonde@gmail.com>
2017-04-09 23:43:47 +0100

    Graph

--- src/Graph.h
+++ src/Graph.h
@@ -274,33 +274,121 @@ public:
 	 *
 	 * @param real true if it is possible for cars to drive in the edge direction, false otherwise
 	 *
-	 * @return true if the edge is sucessfully added false otherwise
+	 * @return true if the edge is successfully added false otherwise
 	 *
 	 */
 	bool addEdge(int sourcID, int destID, double w, Road * road, bool real);
 
 	/**
-	 * @brief removes the
+	 * @brief removes the vertex pointed bt v
 	 *
-	 * @param v pointer to the vertex to be added
+	 * @param v pointer to the vertex to be removed
 	 *
 	 */
 	bool removeVertex(Vertex * v);
+	/**
+	 * @brief removes the edge between the vertexs pointed by sourc and dest
+	 *
+	 * @param sourc pointer to the source vertex
+	 *
+	 * @param dest pointer to the dest vertex
+	 *
+	 * @return true if the edge was successfully removed, false otherwise
+	 *
+	 */
 	bool removeEdge(Vertex * sourc, Vertex * dest);
+	/**
+	 * @brief returns a vector containing all the vertex of the graph
+	 *
+	 * @return VertexSet
+	 *
+	 */
 	vector<Vertex *> getVertexSet();
+	/**
+	 * @brief return the total number of vertex in the graph
+	 *
+	 * @return number of vertex in the graph
+	 *
+	 */
 	int getNumVertex() const;
 
-	void dfs(Vertex * origin); //algoritmo que percorre grafo em profundidade; retorna um vetor so com os vertices que sao alcançaveis
-	void dfsAux(Vertex *v); //auxiliar de dfs() que altera o atributo "visited" dos vertices quando estes sao atingidos
-	Vertex* getVertex(long lon, long lat) const;
+	/**
+	 * @brief algorithm to explore the graph in depth setting the parameter visited to true in all vertex analyzed
+	 *
+	 * @param origin pointer to the first vertex to be explored
+	 *
+	 */
+	void dfs(Vertex * origin);
+	/**
+	 * @brief auxiliary method to the dfs algorithm, that is responsible for changing the visited parameter
+	 *
+	 * @param v pointer to the vertex to be analyzed
+	 *
+	 */
+	void dfsAux(Vertex *v);
+	/**
+	 * @brief gets the vertex with the number identification id
+	 *
+	 * @param id id of the vertex to be returned
+	 *
+	 * @param pointer to the vertex with the specified id
+	 *
+	 */
 	Vertex * getVertex(long id);
-	void resetIndegrees();
-	vector<Vertex*> getSources(); //retorna vetor com possiveis vertices origem usados como ponto de começo em algoritmos de ordenacao, ou seja, vertices com indegree 0
+	/**
+	 * @brief returns a vector containing all vertex that aren't the destination of any edge
+	 *
+	 * @return vector containing pointer to vertex with indegree =0
+	 *
+	 */
+	vector<Vertex*> getSources();
+	/**
+	 * @brief returns a vector containing all vertex in the path from origin vertex to dest vertex
+	 *
+	 * @param origin pointer to the source vertex
+	 *
+	 * @param dest pointer to the destination vertex
+	 *
+	 * @return vector of pointer to the vertex between origin and dest
+	 *
+	 */
 	vector<Vertex*> getPath(Vertex* origin, Vertex* dest);
+	/**
+	 * @brief algorithm to calculate the shortest path from a source vertex to all the other vertex
+	 *  on the map by foot (assumes that every edge is bidirectional)
+	 *
+	 * @param v pointer to the origin vertex
+	 *
+	 */
 	void dijkstraShortestPathByFoot(Vertex * v);
+	/**
+	 * @brief algorithm to calculate the shortest path from a source vertex to all the other vertex
+	 *  on the map by car (only analyzes the edges that belong to the original map)
+	 *
+	 * @param v pointer to the origin vertex
+	 *
+	 */
 	void dijkstraShortestPathByCar(Vertex * v);
+	/**
+	 * @brief returns all the vertex that are in the designated street using a dfs algorithm
+	 *
+	 * @param street street which vertex are returned
+	 *
+	 * @return vector containing the id's from the vertex of the designated street
+	 *
+	 */
 	vector<long> searchStreetNodes(string street);
-	void searchStreetNodes(Vertex * v, string street,
+	/**
+	 * @brief auxiliary method to searchStreetNodes
+	 *
+	 * @param v pointer to the vertex to be analyzed
+	 *
+	 * @param street street whose vertex id's are to be returned
+	 *
+	 * @param vector passed by reference where the id's of the vertex are saved
+	 *
+	 */
+	void searchStreetNodesAux(Vertex * v, string street,
 			vector<long>& streetVertex) const;
 };
 
@@ -314,9 +402,35 @@ class ptrVertex {
 private:
 	Vertex * v;
 public:
+	/**
+	 * @brief ptrVertex constructor
+	 *
+	 * @param v pointer to the vertex
+	 *
+	 */
 	ptrVertex(Vertex * v);
+	/**
+	 * @brief gets the vertex pointer
+	 *
+	 * @return pointer to the vertex stored in ptrVertex
+	 *
+	 */
 	Vertex * getNode() const;
+	/**
+	 * @brief changes the vertex pointed by v
+	 *
+	 * @param v pointer to ththe new vertex
+	 *
+	 */
 	void setNode(Vertex * v);
+	/**
+	 * @brief overload of the < operator
+	 *
+	 * @param v2 const reference of the vertex to be compared
+	 *
+	 * @return true if the vertex pointed by v distance is higher than the vertex pointed by v2 distance
+	 *
+	 */
 	bool operator<(const ptrVertex &v2) const;
 };
 
@@ -325,11 +439,51 @@ private:
 	Vertex * v;
 	double dist;
 public:
+	/**
+	 * @brief closestVertex constructor
+	 *
+	 * @param v pointer to  Vertex that represents a park
+	 *
+	 * @param dist distance to the park represented by vertex v
+	 *
+	 */
 	closestVertex(Vertex * v, double dist);
+	/**
+	 * @brief gets the vertex pointer to the park
+	 *
+	 * @return pointer to vertex v
+	 *
+	 */
 	Vertex * getNode() const;
+	/**
+	 * @brief changes the value pointed by v
+	 *
+	 * @param v pointer to the vertex that is going to replace v
+	 *
+	 */
 	void setNode(Vertex * v);
+	/**
+		 * @brief gets the distance of v to the src vertex
+		 *
+		 * @return dist
+		 *
+		 */
 	double getDist() const;
+	/**
+		 * @brief changes the distance of the vertex pointed by v
+		 *
+		 * @param dist new distance value
+		 *
+		 */
 	void setDist(double dist);
+	/**
+		 * @brief overload of the < operator
+		 *
+		 * @param v2 vertex to be compared to v
+		 *
+		 * @return true if v distance is higher than v2 distance, false otherwise
+		 *
+		 */
 	bool operator<(const closestVertex &v2) const;
 };
 
@@ -338,11 +492,53 @@ private:
 	Vertex * v;
 	double price;
 public:
+	/**
+		 * @brief cheapestVertex constructor
+		 *
+		 * @param v pointer to the vertex that represents a park
+		 *
+		 * @param price price of the the park represented by vertex poitner v
+		 *
+		 *
+		 */
 	cheapestVertex(Vertex * v, double price);
+	/**
+		 * @brief gets a pointer to the vertex v
+		 *
+		 * @return pointer to the vertex (car park)
+		 *
+		 */
 	Vertex * getNode() const;
+	/**
+		 * @brief changes the value pointed by v
+		 *
+		 * @param v pointer to the vertex that is going to replace v
+		 *
+		 */
 	void setNode(Vertex * v);
+	/**
+		 * @brief gets the price of the car park represented by v
+		 *
+		 * @return price
+		 *
+		 */
 	double getPrice() const;
+	/**
+		 * @brief changes the price of the car park
+		 *
+		 * @param price new price
+		 *
+		 */
 	void setPrice(double price);
+
+	/**
+		 * @brief overload of the < operator
+		 *
+		 * @param v2 vertex to be compared to v
+		 *
+		 * @return true if v price is higher than v2 price, false otherwise
+		 *
+		 */
 	bool operator<(const cheapestVertex &v2) const;
 };
 
